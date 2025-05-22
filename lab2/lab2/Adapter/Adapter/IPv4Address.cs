03164a96c85f4495eeb4784f5cdbbca26ff64561
Anatol069 <119695418+Anatol069@users.noreply.github.com>
2025-05-22 15:41:06 +0300

    Add files via upload

---
+++ lab2/lab2/Adapter/Adapter/IPv4Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+      public class IPv4Address :IIPAddress
+    {
+        //campul pt stocarea adresei ipv4
+        private string _address;
+
+        public IPv4Address()
+        {}
+        //primeste o adresa si o stocheaza in ipv4
+        public IPv4Address(string address)
+        {
+            _address = address;
+        }
+        //returneaza adresa stocata
+        public string GetAddress()
+        {
+            return _address;
+        }
+
+        
+    }
+}
